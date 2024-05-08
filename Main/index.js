920b81948989932393399b4f432520124c488d8b
fhubert1 <f.hubert.712@gmail.com>
2024-05-08 10:54:05 -0400

    add prompts and quit function

--- Main/index.js
+++ Main/index.js
@@ -20,6 +20,90 @@ function init() {
         );
 
     // TODO: execute prompts
-    //loadPrompts();
+    loadPrompts(); 
 
 }
+
+function loadPrompts() {
+
+    prompt([
+        {
+          type: "list",
+          name: "choice",
+          message: "What would you like to do?",
+          choices: [
+            {
+                name: "View All Departments",
+                value: "VIEW_DEPARTMENTS"
+            },
+            {
+                name: "View All Roles",
+                value: "VIEW_ROLES"
+            },
+            {
+              name: "View All Employees",
+              value: "VIEW_EMPLOYEES"
+            },
+            {
+                name: "Add Department",
+                value: "ADD_DEPARTMENT"
+            },
+            {
+                name: "Add Role",
+                value: "ADD_ROLE"
+            },
+            {
+              name: "Add Employee",
+              value: "ADD_EMPLOYEE"
+            },
+            {
+              name: "Update Employee Role",
+              value: "UPDATE_EMPLOYEE_ROLE"
+            },
+            {
+              name: "Quit",
+              value: "QUIT"
+            }
+          ]
+        }
+    ]).then(res => {
+        const choice = res.choice;
+        // Call the appropriate function depending on what the user chose
+        switch (choice) {
+            case "VIEW_DEPARTMENTS":
+                viewDepartments();
+            break;
+            case "VIEW_ROLES":
+                viewRoles();
+            break;
+            case "VIEW_EMPLOYEES":
+                viewEmployees();
+            break;
+            case "ADD_DEPARTMENT":
+                addDepartment();
+            break;
+            case "ADD_ROLE":
+                addRole();
+            break;
+            case "ADD_EMPLOYEE":
+                addEmployee();
+            break;
+            case "UPDATE_EMPLOYEE_ROLE":
+                updateEmployeeRole();
+            break;
+            default:
+                quit();
+            break;  
+        }
+    })
+ }
+
+
+
+
+ // Exit the application
+function quit() {
+    console.log("Thank you for using T-Time Employee Tracker - Goodbye!");
+    process.exit();
+  }
+  
